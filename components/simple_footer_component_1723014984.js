fb366a326fde9349d00490b046430f77111ba767
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-07 07:16:24 +0000

    Replace placeholders across project

--- components/simple_footer_component_1723014984.js
+++ components/simple_footer_component_1723014984.js
@@ -9,13 +9,13 @@ Vue.component("simple_footer_component_1723014984", {
                     <div class="flex">
                         <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                             <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                            STARTER_TEMPLATE_APPLICATION_NAME
+                            MobileCraft - Build Your Dream Device
                         </a>
                     </div>
                 </div>
                 <div class="flex">
                     <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                        STARTER_TEMPLATE_FOOTER_CONTENT
+                        Unleash your creativity and construct cutting-edge mobile devices with MobileCraft. Explore endless possibilities, from sleek designs to innovative features. Join our community of passionate builders and bring your mobile masterpieces to life.
                     </div>
                 </div>
             </div>
